3dd5ce6316d0fb2ff7c33500a426235879f88447
krystiano13 <krystiano13@interklasa.pl>
2021-06-12 14:40:14 +0200

    test

---
+++ precache-manifest.7b8c83672a0b86c65d989e0bdc293a91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b542e53ac5fee493de7fb48e67bf19b",
+    "url": "/angielski/index.html"
+  },
+  {
+    "revision": "645fb343033c66beb648",
+    "url": "/angielski/static/css/main.6cd3b0b8.chunk.css"
+  },
+  {
+    "revision": "27fbb77199ded4b20f74",
+    "url": "/angielski/static/js/2.5169edc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/angielski/static/js/2.5169edc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645fb343033c66beb648",
+    "url": "/angielski/static/js/main.231caa7f.chunk.js"
+  },
+  {
+    "revision": "7dcca81d79beafedb17f",
+    "url": "/angielski/static/js/runtime-main.8dedba12.js"
+  }
+]);
